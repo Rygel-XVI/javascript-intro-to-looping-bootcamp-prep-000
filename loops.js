e6007f4da4bf82611fbae588a24431c44d4f3053
Annette Michaels <micha232@umn.edu>
2017-10-03 06:53:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,5 @@
 function forLoop(array){
-  console.log("hi")
-  for(var i = 0; i < 25; i++) {
+  for(var i = 0; i =< 25; i++) {
     console.log("variable i = " + i)
     if(i===1) {
       array.push(`I am 1 strong loop`)
