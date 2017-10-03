63dfcd7225697114584305bc6540888271936d79
Annette Michaels <micha232@umn.edu>
2017-10-03 06:46:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,11 @@
 function makeArray(array){
   for(var i = 0; i < 25; i++) {
-    console.log(i)
+    console.log("variable i = " + i)
     if(i===1) {
       array.push(`I am 1 strong loop`)
     } else {
       array.push(`I am ${i} strange loops`)
     }
-
   }
 return array
 }
